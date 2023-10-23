92a77b427296f9bdf7bb2f13d7d0b7888c6fb717
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-10-23 21:52:35 +0200

    added btn to link homepage to api checker

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './homepage.css'
+import { Link } from 'react-router-dom'
 
 function Homepage() {
   return (
-    <div className='homepage-container'>Homepage</div>
+    <div className='homepage-container'>
+      <button><Link to="/apitester">Check API!</Link></button>
+    </div>
   )
 }
 
