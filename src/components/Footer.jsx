0b1d5139ded854d9d79e67d3aeb57aa4f2878eca
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-10-29 11:24:18 +0100

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { FaRegCopyright } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      {/* add more */}
+      <div className="footer">
+          <div className="footer-left">
+              <p>About Us</p>
+              <p>Terms & Conditions</p>
+              <p>Privacy & Cookies Policies</p>
+          </div>
+          <div className="footer-right">
+              <p>2023</p>
+                <div className='copyright-uniLife'>
+                  <FaRegCopyright />
+                <p>Fetch</p>
+              </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
